ecbedaa6ab1da19d4865f33057d94ff6f9d8d88d
Tobias Wilken <tooangel@tooangel.de>
2020-04-19 14:42:16 +0200

    Start with deploy script and ignore events
    Start with a script to deploy servers, currently getting the data from
    pterodactyl.
    
    Handling the first events by ignoring them.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser');
 const {FeedEmitter} = require('rss-emitter-ts');
+const rp = require('request-promise');
 
 const app = express()
 app.use(bodyParser.json());
@@ -9,6 +10,14 @@ const port = 3000
 
 app.post('/github', (req, res) => {
   if (req.headers['x-github-event'] === 'status') {
+    res.status(200).end();
+    return;
+  }
+  if (req.headers['x-github-event'] === 'push') {
+    res.status(200).end();
+    return;
+  }
+  if (req.headers['x-github-event'] === 'issues') {
     res.status(200).end();
     return;
   }
@@ -25,10 +34,26 @@ module.exports = server;
 
 const feeds = [
     { url: "https://github.com/cuberite/cuberite/commits/master.atom", refresh: 20000, ignoreFirst: true },
-    { url: "https://github.com/equitas-mc/node-controller/commits/master.atom", refresh: 20000, ignoreFirst: true },
 ];
 const emitter = new FeedEmitter();
 emitter.on("item:new", (item) => {
     console.log(`New item: (${item.link})\n${item.title}\n${item.description}\n\n`);
 });
 feeds.forEach((feed) => emitter.add(feed));
+
+const deploy = async function(repository) {
+  var options = {
+    method: 'GET',
+    uri: `https://greaper88.ddns.us:9907/api/application/servers/external/${repository}`,
+    headers: {
+      'Authorization': `Bearer ${process.env.PTERODACTYL_KEY}`,
+      'Content-Type': 'application/json',
+      'Accept': 'Application/vnd.pterodactyl.v1+json',
+    },
+    json: true,
+  };
+  const response = await rp(options);
+  console.log(JSON.stringify(response, null, 2));
+}
+
+deploy('eu-server-skyblock-egg');
