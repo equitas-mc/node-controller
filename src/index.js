65bd43f82d7fd96c871d452af4c63a584a5c59db
Tobias Wilken <tooangel@tooangel.de>
2020-04-19 14:31:10 +0200

    Add feed reader for cuberite commits
    To be able to listen to new commits on the cuberite repository, the atom
    feed is used.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser');
+const {FeedEmitter} = require('rss-emitter-ts');
 
 const app = express()
 app.use(bodyParser.json());
@@ -7,8 +8,13 @@ app.use(bodyParser.json());
 const port = 3000
 
 app.post('/github', (req, res) => {
+  if (req.headers['x-github-event'] === 'status') {
+    res.status(200).end();
+    return;
+  }
   console.log(req.headers);
   console.log(req.body);
+  console.log(req.headers['x-github-event']);
   res.send('Hello World!')
 })
 
@@ -16,3 +22,13 @@ const server = app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`)
 })
 module.exports = server;
+
+const feeds = [
+    { url: "https://github.com/cuberite/cuberite/commits/master.atom", refresh: 20000, ignoreFirst: true },
+    { url: "https://github.com/equitas-mc/node-controller/commits/master.atom", refresh: 20000, ignoreFirst: true },
+];
+const emitter = new FeedEmitter();
+emitter.on("item:new", (item) => {
+    console.log(`New item: (${item.link})\n${item.title}\n${item.description}\n\n`);
+});
+feeds.forEach((feed) => emitter.add(feed));
